fc4213ed1c608b5774aed49c0242d4f73e221449
Alexey Malov <alexey.malov@ispringsolutions.com>
2015-11-14 00:32:22 +0300

    Подмержил lab2 из oop2015, и улучшил код

---
+++ lab2/task1/VectorProcessor.cpp
@@ -0,0 +1,33 @@
+﻿#include "stdafx.h"
+#include "VectorProcessor.h"
+#include <algorithm>
+#include <numeric>
+
+using namespace std;
+
+void ProcessVector(std::vector<double> & numbers)
+{
+	auto IsPositive = [](double number){return number > 0; };
+
+	size_t numberOfPositives = 0;
+
+	// безымянаня функция, суммирующая только положительные числа и ведущая их подсчет
+	auto addIfPositive = [&numberOfPositives](double acc, double current) {
+		if (current > 0.0)
+		{
+			++numberOfPositives;
+			return acc + current;
+		}
+		return acc;
+	};
+
+	auto sumOfPositives = accumulate(numbers.begin(), numbers.end(), 0.0, addIfPositive);
+
+	double avg = (numberOfPositives > 0) ? sumOfPositives / numberOfPositives : 0.0;
+
+	// TODO: Используй силу алгоритма std::transform, Люк, вместо этого цикла
+	for (auto &number : numbers)
+	{
+		number += avg;
+	}
+}
