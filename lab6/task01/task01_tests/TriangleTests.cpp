d0dd2d257263a118b52dfa9132317b050d76b182
Alexey Malov <alexey.malov@ispringsolutions.com>
2016-01-28 22:26:04 +0300

    Реализованы проверки на существование вырожденных треугольников

--- lab6/task01/task01_tests/TriangleTests.cpp
+++ lab6/task01/task01_tests/TriangleTests.cpp
@@ -41,6 +41,15 @@ void ExpectConstructorFailure(double side1, double side2, double side3, const st
 	}, expectedDescription);
 }
 
+void TriangleExists(double side1, double side2, double side3)
+{
+	BOOST_REQUIRE_NO_THROW(CTriangle(side1, side2, side3));
+	CTriangle t(side1, side2, side3);
+	BOOST_CHECK_EQUAL(t.GetSide1(), side1);
+	BOOST_CHECK_EQUAL(t.GetSide2(), side2);
+	BOOST_CHECK_EQUAL(t.GetSide3(), side3);
+}
+
 BOOST_AUTO_TEST_SUITE(Triangle)
 	BOOST_AUTO_TEST_CASE(cannot_have_negative_sides)
 	{
@@ -63,6 +72,26 @@ BOOST_AUTO_TEST_SUITE(Triangle)
 		ExpectConstructorFailure<domain_error>(4, 1, 2, "Side 1 can not be greater than sum of side 2 and side 3");
 		ExpectConstructorFailure<domain_error>(1, 4, 2, "Side 2 can not be greater than sum of side 1 and side 3");
 		ExpectConstructorFailure<domain_error>(1, 2, 4, "Side 3 can not be greater than sum of side 1 and side 2");
+	}
+
+	BOOST_AUTO_TEST_CASE(can_be_singular)
+	{
+		TriangleExists(0, 0, 0);
+
+		TriangleExists(1, 1, 2);
+		TriangleExists(1, 2, 1);
+		TriangleExists(2, 1, 1);
+
+		TriangleExists(0, 1, 1);
+		TriangleExists(1, 0, 1);
+		TriangleExists(1, 1, 0);
+
+		TriangleExists(1, 2, 3);
+		TriangleExists(1, 3, 2);
+		TriangleExists(2, 3, 1);
+		TriangleExists(2, 1, 3);
+		TriangleExists(3, 1, 2);
+		TriangleExists(3, 2, 1);
 	}
 
 	struct when_created_
