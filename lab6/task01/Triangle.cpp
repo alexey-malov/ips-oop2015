dcc71c13de918e9384c3a881e24a8273fcb291ef
Alexey Malov <alexey.malov@ispringsolutions.com>
2016-01-28 21:43:02 +0300

    Сделали невозможным создание треугольника с отрицательными длинами сторон

--- lab6/task01/Triangle.cpp
+++ lab6/task01/Triangle.cpp
@@ -1,12 +1,26 @@
 #include "stdafx.h"
 #include "Triangle.h"
+#include <stdexcept>
+#include <string>
+
+using namespace std;
+
+const double & AssureNotNegative(const double & value, const string & argName = string())
+{
+	if (value >= 0)
+	{
+		return value;
+	}
+	throw invalid_argument(argName.empty() ? "Argument" : argName + " can not be negative.");
+}
+
+#define ASSURE_NOT_NEGATIVE(value) AssureNotNegative((value), #value)
 
 CTriangle::CTriangle(double side1, double side2, double side3)
-	: m_side1(side1)
-	, m_side2(side2)
-	, m_side3(side3)
+	: m_side1(ASSURE_NOT_NEGATIVE(side1))
+	, m_side2(AssureNotNegative(side2, "Side 2"))
+	, m_side3(AssureNotNegative(side3, "Side 3"))
 {
-
 }
 
 double CTriangle::GetSide1()const
--- lab6/task01/task01_tests/TriangleTests.cpp
+++ lab6/task01/task01_tests/TriangleTests.cpp
@@ -1,7 +1,63 @@
 ﻿#include "stdafx.h"
 #include "../Triangle.h"
 
+using namespace std;
+
+template <typename Ex, typename Fn>
+void ExpectException(Fn && fn, const string & expectedDescription)
+{
+	// Проверяем, что вызов fn() выбросит исключение типа Ex 
+	// с описанием, равным expectedDescription
+	BOOST_CHECK_EXCEPTION(fn(), Ex, [&](const Ex& e){
+		return e.what() == expectedDescription; 
+	});
+
+	/*
+	try
+	{
+
+		fn(); // Выполняем действие, от которого ожидаем выброс исключения
+		// Если не выбросило, то тест провалился
+		BOOST_ERROR("No exception thrown");
+	}
+	catch (const Ex & e) // Ловим исключение типа Ex (или его подкласс)
+	{
+		// Проверяем, что исключение будет иметь ожидаемое сообщение
+		BOOST_CHECK_EQUAL(expectedDescription, e.what());
+	}
+	catch (...)
+	{
+		// Если выбросили иное исключение, то это также ошибка
+		BOOST_ERROR("Unexpected exception");
+	}
+	*/
+}
+
+template <typename Ex>
+void ExpectConstructorFailure(double side1, double side2, double side3, const string & expectedDescription)
+{
+	ExpectException<Ex>([&]{
+		CTriangle(side1, side2, side3); 
+	}, expectedDescription);
+}
+
 BOOST_AUTO_TEST_SUITE(Triangle)
+	BOOST_AUTO_TEST_CASE(cannot_have_negative_sides)
+	{
+		ExpectConstructorFailure<invalid_argument>(-1, 2, 3, "side1 can not be negative.");
+		ExpectConstructorFailure<invalid_argument>(1, -2, 3, "Side 2 can not be negative.");
+
+		ExpectException<invalid_argument>([]{
+			CTriangle(1, 2, -3);
+		}, "Side 3 can not be negative.");
+
+		/*
+		BOOST_CHECK_THROW(CTriangle(-1, 2, 3), invalid_argument);
+		BOOST_CHECK_THROW(CTriangle(1, -2, 3), invalid_argument);
+		BOOST_CHECK_THROW(CTriangle(1, 2, -3), invalid_argument);
+		*/
+	}
+
 	struct when_created_
 	{
 		CTriangle t = CTriangle(3, 4, 5);
@@ -12,6 +68,6 @@ BOOST_AUTO_TEST_SUITE(Triangle)
 			BOOST_CHECK_EQUAL(t.GetSide1(), 3);
 			BOOST_CHECK_EQUAL(t.GetSide2(), 4);
 			BOOST_CHECK_EQUAL(t.GetSide3(), 5);
-	}
+		}
 	BOOST_AUTO_TEST_SUITE_END()
 BOOST_AUTO_TEST_SUITE_END()
